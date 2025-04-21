ce1de5abef24f6c6fb4a19e8ac5742a7ab2de33c
el houssaine el gamouz <elhoussaineelgamouz@gmail.com>
2025-04-21 14:39:01 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by el houssaine el gamouz on 21/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by el houssaine el gamouz on 21/4/2025.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
